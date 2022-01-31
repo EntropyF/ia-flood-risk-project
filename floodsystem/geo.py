b899abc1d3c9caf133c6cb88c4a1bd4242ac70d8
EntropyF <fujiajun73@gmail.com>
2022-01-31 12:23:26 +0000

    complete 1B

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,20 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+from haversine import haversine
+#from . import datafetcher
+#from .station import MonitoringStation
+#from floodsystem.stationdata import build_station_list
+
+names = []
+distance = []
+def stations_by_distance(stations, p):
+
+    for station in stations:
+        names.append(station.name)
+        distance.append(haversine(p, station.coord))
+    
+    turples = list(zip(names, distance))
+    turples = sorted_by_key(turples,1)
+    return turples
