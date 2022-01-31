61e469e269f2f1e7df3613688d94314a171b8dae
EntropyF <fujiajun73@gmail.com>
2022-01-31 12:32:06 +0000

    complete 1B

--- Task1B.py
+++ Task1B.py
@@ -7,6 +7,7 @@ def run():
 
     x = stations_by_distance(stations, p)
     print(x[:10])
+    print(x[-10:])
 
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
