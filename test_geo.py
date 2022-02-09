9c9eed5f6a4409e8e448a08e0e6c634de02e3679
Muhan Li <ml2016@cam.ac.uk>
2022-02-09 03:02:46 +0000

    fix some error

--- test_geo.py
+++ test_geo.py
@@ -12,7 +12,7 @@ s_id1,s_id2,s_id3 = "test-s-id1","test-s-id2","test-s-id3"
 m_id1,m_id2,m_id3 = "test-m-id1","test-m-id2","test-m-id3"
 label1,label2,label3 = "station1","station2","station3"
 coord1,coord2,coord3 = (-3.0, 4.0),(1.0,0.0),(0.0,3.0)
-trange1,trange2,trange3 = (-2.3, 3.4),(-3.0,3.0),(2.4,6.8)
+trange1,trange2,trange3 = (-5.0, 7.4),(-3.0,3.0),(2.4,6.8)
 river1,river2,river3 = "River X","River Y","River X"
 town1,town2,town3 = "Town X","Town Y","Town Z"
 
@@ -24,7 +24,7 @@ s = [s1,s2,s3]
 
 def test_stations_by_distance():
     """Test stations sorted by distance"""
-    test = stations_by_distance(s, 0.0)
+    test = stations_by_distance(s,(0.0,0.0))
     for i in len(test) - 1:
         stations_by_distance[i][1] < stations_by_distance[i+1][1]
     assert True
@@ -33,16 +33,16 @@ def test_stations_by_distance():
 
 def test_stations_within_radius():
     """Test stations within certain radius"""
-    assert stations_within_radius(s, (0.0,0.0), 2.0) == ["station2"]
+    assert stations_within_radius(s, (0.0,0.0), 2.0) == 'station2'
 
 def test_rivers_with_station():
     """Test rivers with a monitoring station (no duplicate entries)"""
-    assert test_rivers_with_station(s) == {"River X", "River Y"}
+    assert test_rivers_with_station(s) == {'River X', 'River Y'}
 
 def test_stations_by_river():
     """Test rivers (keys) mapping to stations (values)"""
-    assert stations_by_river(s) == {s1: "River X", s2: "River Y", s3: "River X"}
+    assert stations_by_river(s) == {'River X': ['station1', 'station3'], 'River Y': ['station2']}
 
 def test_rivers_by_station_number():
     """Test rivers with the greatest number of stations"""
-    assert rivers_by_station_number(s,2) == ("River X", 2)
+    assert rivers_by_station_number(s,2) == ('River X', 2)
