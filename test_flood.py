1232e80ad6f3b42b6884f49c4edbf96f493a4068
Muhan Li <ml2016@cam.ac.uk>
2022-02-28 01:10:39 +0000

    fix assertion error

--- test_flood.py
+++ test_flood.py
@@ -10,11 +10,11 @@ def test_stations_level_over_threshold():
     test_stations = stations_level_over_threshold(stations, 0.8)
     for x in test_stations:
         assert x[1] > 0.8
-    for x in range(len(test_stations)):
-        assert test_stations[x][1] <= test_stations[x-1][1]
+    for x in range(len(test_stations)-1):
+        assert test_stations[x][1] >= test_stations[x+1][1]
 
 def test_stations_highest_rel_level():
     '''Test N stations with highest relative water level in descending order'''
     test_stations = stations_highest_rel_level(stations,10)
-    for x in range(len(test_stations)):
-        assert test_stations[x][1] <= test_stations[x-1][1]
+    for x in range(len(test_stations)-1):
+        assert test_stations[x][1] >= test_stations[x+1][1]
