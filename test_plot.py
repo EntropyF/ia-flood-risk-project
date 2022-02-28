7b7310d4ff3f228d1b7e5e4b650c2464757d0011
Muhan Li <ml2016@cam.ac.uk>
2022-02-28 01:11:04 +0000

    add a line

--- test_plot.py
+++ test_plot.py
@@ -1,4 +1,5 @@
 import datetime
+from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list
 from floodsystem.plot import plot_water_levels, plot_water_level_with_fit
 
@@ -9,4 +10,5 @@ def test_plot_water_levels():
     assert plot_water_levels(stations[0], 2, stations[0].typical_range)
 
 def test_plot_water_level_with_fit():
+    '''Test polynomail plot of water level - time'''
     assert plot_water_level_with_fit(stations[0], 2, stations[0].typical_range, 3)
