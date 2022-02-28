b470db3a8bf0e8ca31d0cb85c685b934dfb036fe
Muhan Li <ml2016@cam.ac.uk>
2022-02-28 00:08:19 +0000

    Complete test_plot - test of 2E and 2F

---
+++ test_plot.py
@@ -0,0 +1,12 @@
+import datetime
+from floodsystem.stationdata import build_station_list
+from floodsystem.plot import plot_water_levels, plot_water_level_with_fit
+
+stations = build_station_list()
+
+def test_plot_water_levels():
+    '''Test water level - time plot'''
+    assert plot_water_levels(stations[0], 2, stations[0].typical_range)
+
+def test_plot_water_level_with_fit():
+    assert plot_water_level_with_fit(stations[0], 2, stations[0].typical_range, 3)
